c8f807d0d359c6b328a00b6930b8665ed7cc0b3c
BenedictusNC <nicholas.christian2006@gmail.com>
2026-06-03 12:20:45 +0700

    pertemuan 13

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PegawaiDBController;
+
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari',[PegawaiDBController::class, 'cari']);
+Route::get('/', function () {
+    return view('menu');
+});
+Route::get('/pertemuan1', function () {
+    return view('pertemuan1');
+});
+Route::get('/pertemuan2', function () {
+    return view('pertemuan2');
+});
+Route::get('/pertemuan3', function () {
+    return view('pertemuan3');
+});
+Route::get('/pertemuan4', function () {
+    return view('pertemuan4');
+});
+Route::get('/pertemuan5', function () {
+    return view('pertemuan5');
+});
+Route::get('/pertemuan6', function () {
+    return view('pertemuan6');
+});
+
